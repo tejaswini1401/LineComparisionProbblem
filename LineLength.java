0aacc3c6bc5842d0fa716c4a1c54229fce688598
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-08-06 12:14:22 +0530

    Check Equality of Line length

--- LineLength.java
+++ LineLength.java
@@ -5,6 +5,7 @@ public class LineLength {
         
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the coordinates for the first line:");
         System.out.print("Enter x1: ");
         double x1 = sc.nextDouble();
         System.out.print("Enter y1: ");
@@ -15,9 +16,29 @@ public class LineLength {
         System.out.print("Enter y2: ");
         double y2 = sc.nextDouble();
 
-        double distance = calculateDistance(x1, y1, x2, y2);
+        double distance1 = calculateDistance(x1, y1, x2, y2);
 
-        System.out.println("The length of the line is: " + distance);
+        System.out.println("Enter the coordinates for the second line:");
+        System.out.print("Enter x3: ");
+        double x3 = sc.nextDouble();
+        System.out.print("Enter y3: ");
+        double y3 = sc.nextDouble();
+
+        System.out.print("Enter x4: ");
+        double x4 = sc.nextDouble();
+        System.out.print("Enter y4: ");
+        double y4 = sc.nextDouble();
+
+        double distance2 = calculateDistance(x3, y3, x4, y4);
+
+        System.out.println("The length of the first line is: " + distance1);
+        System.out.println("The length of the second line is: " + distance2);
+
+        if (distance1 == distance2) {
+            System.out.println("Both lines are of equal length.");
+        } else {
+            System.out.println("The lines are of different lengths.");
+        }
 
         sc.close();
     }
